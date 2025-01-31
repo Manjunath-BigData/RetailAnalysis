4b92823e5753d21b4b3600f1e147608b54ca7ff9
Manjunath-BigData <manjujituri.forex@gmail.com>
2025-01-31 11:02:39 +0530

    adding initial set of files

---
+++ conftest.py
@@ -0,0 +1,15 @@
+
+import pytest
+from lib.Utils import get_spark_session
+
+#@pytest.fixture
+#def spark():
+#    return get_spark_session("LOCAL")
+
+
+@pytest.fixture
+def spark():
+    "creates spark session"
+    spark_session =  get_spark_session("LOCAL")
+    yield spark_session
+    spark_session.stop()
